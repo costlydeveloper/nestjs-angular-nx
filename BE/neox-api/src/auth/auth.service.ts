15b460d1c173f0626b28553c71c0bfcbe5b87fc7
Boris Jeniček <boris.jenicek@gmail.com>
2023-10-11 23:15:08 +0200

    refactor(be): rename access token

--- BE/neox-api/src/auth/auth.service.ts
+++ BE/neox-api/src/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.username, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 }
