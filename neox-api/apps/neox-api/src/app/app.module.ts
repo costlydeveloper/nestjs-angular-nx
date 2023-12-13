e1e35c9a58678c261de0a53f6031d07fb1f35a9d
boris jenicek <boris.jenicek@gmail.com>
2023-12-13 22:57:47 +0100

    ci: package.json

--- neox-api/apps/neox-api/src/app/app.module.ts
+++ neox-api/apps/neox-api/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppService } from './app.service';
         host: '127.0.0.1',
         port: 3306,
         username: 'root',
-        password: 'root',
+        password: '3007',
         database: 'main_db',
         entities: [User],
         synchronize: true,
