54b7940bac9f069ec49f8d5bd88a0afa0be81e6b
boris jenicek <boris.jenicek@gmail.com>
2024-01-17 16:25:19 +0100

    refactor(be): swagger setup

--- neox-api/libs/endpoints/src/lib/tasks/dto/get-tasks-filter.dto.ts
+++ neox-api/libs/endpoints/src/lib/tasks/dto/get-tasks-filter.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
 
 export class GetTasksFilterDto {
   @IsOptional()
   @IsEnum(TaskStatus)
+  @ApiProperty()
   status?: TaskStatus;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   search?: string;
 }
--- neox-api/libs/endpoints/src/lib/users/users.controller.ts
+++ neox-api/libs/endpoints/src/lib/users/users.controller.ts
@@ -2,28 +2,21 @@ import { BaseController } from '@neox-api/shared/common';
 import { Serialize } from '@neox-api/shared/utils';
 
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiBody, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthCredentialsDto } from '../auth/dto/auth-credentials.dto';
-import { CreateUserDto, UserDto } from './dto';
+import { UserDto } from './dto';
 import { IUser, IUserOmitPassword } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 @Serialize(UserDto)
+@ApiTags('Users')
+@ApiBearerAuth()
 export class UsersController extends BaseController<IUser> {
   constructor(private readonly usersService: UsersService) {
     super(usersService);
   }
 
-  @ApiBody({
-    type: CreateUserDto,
-    description: 'Json structure for user object',
-  })
-  @ApiResponse({
-    status: 201,
-    description: 'The record has been successfully created.',
-  })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @Post()
   create(
     @Body() authCredentialsDto: AuthCredentialsDto,
