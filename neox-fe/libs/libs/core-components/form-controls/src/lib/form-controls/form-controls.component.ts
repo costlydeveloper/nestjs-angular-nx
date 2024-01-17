27fd917d0a5eebe210999153ca8a4206e8149bc6
boris jenicek <boris.jenicek@gmail.com>
2024-01-17 10:33:00 +0100

    refactor(fe): add modals

---
+++ neox-fe/libs/libs/core-components/form-controls/src/lib/form-controls/form-controls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tl-form-controls',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-controls.component.html',
+  styleUrl: './form-controls.component.scss',
+})
+export class FormControlsComponent {}
