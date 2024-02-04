25c2453aa60627e2ac76efcc3f3948db2a0416aa
boris jenicek <boris.jenicek@gmail.com>
2024-02-04 01:12:12 +0100

    feat(fe): add small upgrades

--- neox-fe/libs/core-components/form/src/dependencies/index.ts
+++ neox-fe/libs/core-components/form/src/dependencies/index.ts
@@ -1,3 +1,4 @@
 export * from './dynamic-form-control.interface';
 export * from './form-layout.interface';
 export * from './dynamic-form-control.enum';
+export * from './form-compact-output.types';
