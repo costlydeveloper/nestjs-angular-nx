4fbc0e8ecc3547f4226a65c5f3d6b3de7ea4313b
boris jenicek <boris.jenicek@gmail.com>
2024-02-04 01:12:53 +0100

    refactor(fe): signin form

---
+++ neox-fe/libs/features/views/auth/src/lib/smart-sign-in/sign-in-form.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Validators } from '@angular/forms';
+import {
+  DynamicControlTypeEnum,
+  IDynamicFormControl,
+  IErrorAssociation,
+  InputType,
+} from '@team-link/form-controls';
+
+export interface ISignInForm {
+  email: string;
+  password: string;
+}
+@Injectable()
+export class SignInFormService {
+  formGeneratorConfig: IDynamicFormControl[] = [
+    {
+      id: 'email',
+      label: {
+        name: 'auth.label.email',
+      },
+      placeholder: 'auth.placeholder.email',
+      titleAtt: 'auth.label.email',
+      type: DynamicControlTypeEnum.INPUT_TEXT,
+      controlConfig: {
+        inputType: InputType.EMAIL,
+      },
+      validators: [
+        {
+          validator: Validators.required,
+          errorAssociation: IErrorAssociation.REQUIRED,
+        },
+      ],
+    },
+    {
+      id: 'password',
+      label: {
+        name: 'auth.label.password',
+      },
+      placeholder: 'auth.placeholder.password',
+      titleAtt: 'auth.placeholder.password',
+      type: DynamicControlTypeEnum.INPUT_TEXT,
+      controlConfig: {
+        inputType: InputType.PASSWORD,
+      },
+      validators: [
+        {
+          validator: Validators.required,
+          errorAssociation: IErrorAssociation.REQUIRED,
+        },
+      ],
+    },
+  ];
+}
