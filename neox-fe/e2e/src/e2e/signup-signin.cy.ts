b5193df714a40ed93e6a8b430e2cbe2e0fabd634
boris jenicek <boris.jenicek@gmail.com>
2024-03-02 11:45:21 +0100

    test(fe): e2e signup

--- neox-fe/e2e/src/e2e/signup-signin.cy.ts
+++ neox-fe/e2e/src/e2e/signup-signin.cy.ts
@@ -3,18 +3,23 @@ import { cleanTestDatabase } from '../helper/clean-test-db.func';
 
 describe('SignUp Test', () => {
   const signUpRoute = '/auth/sign-up';
-
   before(() => {
     cleanTestDatabase();
   });
 
-  it('successfully signs up a new user', () => {
+  it('should signup and capture auth token', () => {
     cy.visit(signUpRoute);
+    cy.intercept('POST', '/api/auth/signup').as('signupRequest');
 
     getByDataE2eId('email-input').type('test@example.com');
     getByDataE2eId('password-input').type('Password123!');
     getByDataE2eId('signup-button').click();
 
     cy.url().should('include', '/dashboard');
+
+    cy.wait('@signupRequest').then((interception) => {
+      expect(interception.response?.body).to.have.property('accessToken');
+      expect(interception.response?.body).to.have.property('refreshToken');
+    });
   });
 });
