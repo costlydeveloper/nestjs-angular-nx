19ba5b4b67527459fd008d6f738abfca5c85162d
boris jenicek <boris.jenicek@gmail.com>
2024-03-12 10:39:34 +0100

    ci(fe): test ci

--- neox-fe/docker-entrypoint.test.sh
+++ neox-fe/docker-entrypoint.test.sh
@@ -7,16 +7,16 @@ set -e
 command=$START_SCRIPT
 
 echo "START_SCRIPT: $START_SCRIPT"
- exit 1
+
 # If START_SCRIPT is set to "run-all", run the tests
 if [ "$START_SCRIPT" = "run-all" ]; then
     echo "Running all tests..."
     # Run unit tests and store the exit code
-      test_status=0
-          npm run test:unit || test_status=$?
-          npm run test:e2e || test_status=$(($test_status | $?))
-          echo "Tests finished with status: $test_status"
-          exit $test_status
+      npm run test:unit && npm run test:e2e
+        # Exit code of the last command executed
+         exitCode=$?
+         echo "Tests finished with exit code: $exitCode"
+          if [ ${exitCode} -ne 0 ]; then exit 1; fi
 else
     echo "Starting application with command: $command"
     # Execute the command, appending necessary Docker-specific arguments
