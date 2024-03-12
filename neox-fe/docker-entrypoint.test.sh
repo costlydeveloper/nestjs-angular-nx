7ef346f9b9b9446b980fa3b1541c4e4dd70f55ae
boris jenicek <boris.jenicek@gmail.com>
2024-03-12 09:53:07 +0100

    ci(fe): test ci

--- neox-fe/docker-entrypoint.test.sh
+++ neox-fe/docker-entrypoint.test.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-# as defined in the START_SCRIPT environment variable
+# Exit script on the first error
+set -e
+
+# As defined in the START_SCRIPT environment variable
 command=$START_SCRIPT
 
-echo $START_SCRIPT
-# If RUN_TESTS is set, run the tests
+echo "START_SCRIPT: $START_SCRIPT"
+
+# If START_SCRIPT is set to "run-all", run the tests
 if [ "$START_SCRIPT" = "run-all" ]; then
     echo "Running all tests..."
-    npm run test:unit && npm run test:e2e
-    exit_code=$?
-    echo "Tests finished with exit code: $exit_code"
-    exit $exit_code
+    npm run test:unit
+    npm run test:e2e
 else
     echo "Starting application with command: $command"
-    exec sh -c "$command --host 0.0.0.0 --disable-host-check"
+    # Execute the command, appending necessary Docker-specific arguments
+    if [ -n "$command" ]; then
+        exec sh -c "$command --host 0.0.0.0 --disable-host-check"
+    else
+        echo "No command specified, exiting..."
+        exit 1
+    fi
 fi
